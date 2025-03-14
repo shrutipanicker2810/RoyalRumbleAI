565173cf1b28fc7e6b58f27a99b812b612bcf725
shrutipanicker2810 <shrutipanicker2810@gmail.com>
2025-03-14 15:52:59 -0400

    created a new directory that will contain files specific to humanoid agents only

---
+++ humanoid-royalrumble/__init__.py
@@ -0,0 +1,13 @@
+# Here, we register the environments. 
+# Example:
+
+# register(
+#     id='RoboSumo-Ant-vs-Ant-v0',
+#     entry_point='robosumo.envs:SumoEnv',
+#     kwargs={
+#         'agent_names': ['ant', 'ant'],
+#         'agent_densities': [13., 13.],
+#         'tatami_size': 2.0,
+#         'timestep_limit': 500,
+#     },
+# )
---
+++ humanoid-royalrumble/envs/__init__.py
@@ -0,0 +1,16 @@
+# We use this file to register environments, using the libraries in 
+# mujoco_env and sumo files
+
+# Sample code:
+
+# from .mujoco_env import MujocoEnv
+# from .sumo import SumoEnv
+
+# from gym.envs.registration import register
+
+# register(
+#     id="RoboSumo-Ant-vs-Ant-v0",
+#     entry_point="robosumo.envs.sumo:SumoEnv",
+# )
+
+# print("RoboSumo environment registered!")
